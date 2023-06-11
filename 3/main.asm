4057ee25feb76e9fe63e6246872a921e9835f3fe
shhhQuiettt <krzysztof.skro@gmail.com>
2023-06-11 14:06:10 +0200

    Sequance of sqare roots

--- 3/main.asm
+++ 3/main.asm
@@ -7,8 +7,8 @@ extern  printf
 extern  scanf
 
 section .data
-	; input   db 'Hello World!', 0
     format  db '%lf', 0
+    format2 db 'sqrt(%f) = %f ', 0xA, 0
     one_eighth  dq 0x3FC0000000000000
 
 section .bss
@@ -23,63 +23,53 @@ main:
     lea     rdi, [format]
     mov     al, 0
     call    scanf wrt ..plt
-    movlpd  xmm1, [end]
+    movlpd  xmm2, [end]
 
-    pxor     xmm2, xmm2
+    pxor     xmm0, xmm0
+
+    ; xmm0 stores incremented value
+    ; xmm1 stores computed square
+    ; xmm2 stores boundry
     loop:
-    ; cmpltsd xmm1, [zero]
-    ; movq rax, xmm0
-    ; cmp rax, 0
-    ; je end_loop
+    ; while xmm1 less than or equal xmm2
+    comisd  xmm0, xmm2
+    ja      end_loop
+    je      end_loop
     
-    ; mov rax,  
-    ; subss xmm1, 
-    
+    sqrtsd  xmm1, xmm0
 
+    ;calling main
+    movq  rax, xmm0
+    push    rax
 
+    movq  rax, xmm1
+    push    rax
+
+    movq  rax, xmm2
+    push    rax
+
+	sub     rsp, 8
+
+    lea     rdi, [format2]
+    mov     al, 2
+    call    printf wrt ..plt
+
+    add     rsp, 8
+
+    pop     rax
+    movq     xmm2, rax
+
+    pop     rax
+    movq    xmm1, rax
+
+    pop     rax
+    movq    xmm0, rax
+    ;;;;
+
+    addsd   xmm0, [one_eighth]
+    ;
+    jmp     loop
     end_loop:
-    ; sqrtsd  xmm0, xmm1
-    ; lea     rdi, [format]
-    ; mov     al, 1
-    ; call    printf wrt ..plt
-
-    ;lea     rdi, [format]
-    ;lea     rsi, [input]
-    ;xor     eax, eax
-    ;call    scanf wrt ..plt
-
-
-	;;   length of the input string
-	;mov rcx, 0
-
-	;lea rsi, [input]
-    ;mov r8, rsi
-    ;loop:
-    ;    cmp byte [r8], 0x0;
-    ;    je loop_end
-
-    ;    inc rcx
-    ;    inc r8
-    ;    jmp loop
-    ;loop_end:
-    ;;
-        
-	;mov rdi, output
-
-	;rep movsb
-
-
-    ;;message - second argument
-    ;mov rdi, output
-    ;push rdi
-    ;;format string  - first argument
-    ;mov rax, format
-    ;push rax
-
-    ;call printf wrt ..plt
-
-    ;;cleaning printf arguments
-    ;add rsp, 16
 
     add rsp, 8
 
